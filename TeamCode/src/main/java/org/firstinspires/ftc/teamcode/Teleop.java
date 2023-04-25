344b560bb78a3d4cf8cd6674369bb3287cddb8d7
Skywalker934 <ledwards26@cathedral-elpaso.org>
2023-04-25 15:14:02 -0600

    Created empty teleop program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
+
+public class Teleop extends BasicOpMode_Iterative {
+}
