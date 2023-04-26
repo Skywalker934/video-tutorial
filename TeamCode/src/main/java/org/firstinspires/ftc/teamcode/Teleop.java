3a9b74888cacaa4fa81ff174ae8a0c3009a1d8be
Skywalker934 <ledwards26@cathedral-elpaso.org>
2023-04-26 14:44:57 -0600

    Created a beta class on the beta branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,4 +3,4 @@ package org.firstinspires.ftc.teamcode;
 import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
 
 public class Teleop extends BasicOpMode_Iterative {
-}
+} 
